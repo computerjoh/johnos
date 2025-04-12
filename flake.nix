8930ecc77e607fa6b9d23a89050b3b6e87ec005e
John <johnkorhel1@gmail.com>
2025-04-11 20:15:23 -0600

    update flake

--- flake.nix
+++ flake.nix
@@ -2,16 +2,24 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
+    unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { self, nixpkgs, ... }:
-  {
+  outputs = { self, nixpkgs, unstable, ... }: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      # Note that you cannot put arbitrary configuration here: the configuration must be placed in the files loaded via modules
       system = "x86_64-linux";
       modules = [
         ./configuration.nix
+        ({ config, pkgs, ... }: {
+          nixpkgs.overlays = [
+            (final: prev: {
+              unstable = import unstable {
+                system = "x86_64-linux";
+              };
+            })
+          ];
+        })
       ];
     };
   };
