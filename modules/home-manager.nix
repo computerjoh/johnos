bdee940ac9aa5585849042f42db95cb7eae629f4
John <johnkorhel1@gmail.com>
2025-05-13 21:01:29 -0600

    add git

--- modules/home-manager.nix
+++ modules/home-manager.nix
@@ -2,12 +2,14 @@
 
 {
   home-manager.users.john = {
-   home.stateVersion = "24.11";
-   programs.home-manager.enable = true;
+    home.stateVersion = "24.11";
+    programs.home-manager.enable = true;
 
-  programs.git = {
-    userName = "John Korhel";
-    userEmail = "john@korhel.net";
-    extraConfig.init.defaultBranch = "main";
+    programs.git = {
+      userName = "John Korhel";
+      userEmail = "john@korhel.net";
+      extraConfig.init.defaultBranch = "main";
+    };
   };
 };
+
