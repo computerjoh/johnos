9d0d0959cbcc4656d0d1258e726dfb171b60caf4
John Korhel <john@korhel.net>
2025-05-23 19:31:52 -0600

    i suck

--- modules/networking.nix
+++ modules/networking.nix
@@ -3,8 +3,6 @@
 {
   networking.hostName = "nixos";
   networking.networkmanager.enable = true;
-  networking.interfaces.enp3s0.wakeOnLan.enable = true;
-  services.tailscale.enable = true;
   services.openssh.enable = true;
   # Firewall Configuration
   networking.firewall = {
