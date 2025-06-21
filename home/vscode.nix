97dfc3acecdd1947ad2e4d7f4c33c0b3d7bb9bba
John Korhel <john@korhel.net>
2025-06-21 09:25:57 -0600

    add back nix ide

--- home/vscode.nix
+++ home/vscode.nix
@@ -3,14 +3,24 @@
     enable = true;
     package = pkgs.vscodium;
     mutableExtensionsDir = true;
-    profiles = {
-      default = {
-        extensions = with pkgs.vscode-extensions; [
-          github.copilot
-          kamadorueda.alejandra
-        ];
-        userSettings = {
-          "editor.formatOnSave" = true;
+
+    profiles.default = {
+      extensions = with pkgs.vscode-extensions; [
+        github.copilot
+        jnoortheen.nix-ide
+      ];
+
+      userSettings = {
+        "editor.formatOnSave" = true;
+        "nix.enableLanguageServer" = true;
+        "nix.serverPath" = "nixd";
+        "nix.diagnostics.ignoreWarnings" = true;
+        "nix.serverSettings" = {
+          "nixd" = {
+            "formatting" = {
+              "command" = ["alejandra"];
+            };
+          };
         };
       };
     };
